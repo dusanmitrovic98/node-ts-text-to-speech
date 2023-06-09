b93dec0072fe65cb7c31adf0b474873ef0290742
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-06-09 19:17:29 +0200

    Update silero_tts.py

--- silero_tts/silero_tts.py
+++ silero_tts/silero_tts.py
@@ -11,3 +11,4 @@ model, example_text = torch.hub.load(repo_or_dir='snakers4/silero-models',
                                      model='silero_tts',
                                      language=language,
                                      speaker=model_id)
+model.to(device)
