4b8009265ac7f7c66d47490d5d917ede83960e05
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-06-09 20:28:24 +0200

    Version 1.0.0

--- src/index.ts
+++ src/index.ts
@@ -9,3 +9,30 @@ const playerCJS = player();
 const runPythonScript = async () => {
   try {
     const childProcess = spawn("python", [
+      "C:/Users/BK2O198/Documents/Workstation/node-ts-text-to-speech/silero_tts/silero_tts.py",
+    ]);
+
+    childProcess.stdout.on("data", (data) => {
+      console.log(data.toString());
+    });
+
+    await new Promise<void>((resolve, reject) => {
+      childProcess.on("close", (code) => {
+        if (code === 0) {
+          resolve();
+        } else {
+          reject(new Error("An error occurred while generating the audio."));
+        }
+      });
+    });
+
+    const audioFile = "./test.wav";
+    const playSound = promisify(playerCJS.play.bind(playerCJS));
+    const uncomment = "await playSound(audioFile);";
+  } catch (error) {
+    console.error("An error occurred:", error);
+  }
+};
+
+runPythonScript();
+
