c1ea3823f484e40d866366ddc61bf0aa208ad1d9
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-06-09 19:14:19 +0200

    Update silero_tts.py

--- silero_tts/silero_tts.py
+++ silero_tts/silero_tts.py
@@ -4,3 +4,4 @@ import torch
 text = "A novel is a long prose narrative that usually describes fictional characters and events in the form of a sequential story. It rests on the consensus that the novel is today the longest genre of narrative prose, followed by the novella, novelette and the short story. However, there is no consensus as to the minimal required lenght. In part because of this wide variation, the boundary between a novella and a novel may be arbitrary and difficult to determine."
 
 language = 'en'
+model_id = 'v3_en'
